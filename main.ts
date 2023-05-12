dccbee3fb67e3a3f55e49d77935e8c5b86523b38
juju2303 <132678792+juju2303@users.noreply.github.com>
2023-05-12 17:36:50 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,8 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
-        basic.showIcon(IconNames.Ghost)
-    } else {
-        basic.showString("Appareil introuvable")
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
-    }
+    basic.showIcon(IconNames.Ghost)
 })
 radio.setGroup(204)
 radio.setTransmitPower(7)
+basic.forever(function () {
+	
+})
